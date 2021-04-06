84492f3981e5e0e86f8885a3dcb1cca1b05ecf7a
xjerabe2 <xjerabe2@node.mendelu.cz>
2021-04-06 14:49:00 +0200

    baseURL switch to prod

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,12 @@
 import axios from 'axios'
-
+/*
 export default axios.create({
     baseURL: 'http://localhost:8000/'
+    
 });
+*/
 
+
+export default axios.create({
+    baseURL: 'https://app-eve.herokuapp.com/'
+});
